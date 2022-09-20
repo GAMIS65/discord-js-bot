eebb149de4668e5573672bc7eafdd9039babae28
GAMIS65 <ivicko05@gmail.com>
2022-09-20 22:41:17 +0200

    Added more intents

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,14 @@
 const { Client, GatewayIntentBits } = require('discord.js');
 require('dotenv').config();
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const client = new Client({ 
+	intents: [
+		GatewayIntentBits.Guilds,
+		GatewayIntentBits.GuildMessages,
+		GatewayIntentBits.MessageContent,
+		GatewayIntentBits.GuildMembers,
+	],
+});
 
 client.once('ready', () => {
 	console.log('i liek kiwis=====');
